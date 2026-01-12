3642c34e2b5c92625897b67b4ebde20c1ae91bf1
Andrew <104359161+Andrew7441@users.noreply.github.com>
2026-01-12 13:00:42 +0200

    Add Cmake Changes

---
+++ out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/andre/source/repos/solid-ecommerce-order-system
+# Build directory: C:/Users/andre/source/repos/solid-ecommerce-order-system/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(AllTests "C:/Users/andre/source/repos/solid-ecommerce-order-system/out/build/x64-Debug/tests.exe")
+set_tests_properties(AllTests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/andre/source/repos/solid-ecommerce-order-system/CMakeLists.txt;71;add_test;C:/Users/andre/source/repos/solid-ecommerce-order-system/CMakeLists.txt;0;")
