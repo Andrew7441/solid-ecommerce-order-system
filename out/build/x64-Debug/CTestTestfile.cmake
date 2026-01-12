3feb92df069f8d5af27722a4d48a680a88eaf062
Andrew <104359161+Andrew7441@users.noreply.github.com>
2026-01-12 13:10:26 +0200

    Add Fetch GoogleTest and fix test linking

--- out/build/x64-Debug/CTestTestfile.cmake
+++ out/build/x64-Debug/CTestTestfile.cmake
@@ -5,4 +5,5 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(AllTests "C:/Users/andre/source/repos/solid-ecommerce-order-system/out/build/x64-Debug/tests.exe")
-set_tests_properties(AllTests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/andre/source/repos/solid-ecommerce-order-system/CMakeLists.txt;71;add_test;C:/Users/andre/source/repos/solid-ecommerce-order-system/CMakeLists.txt;0;")
+set_tests_properties(AllTests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/andre/source/repos/solid-ecommerce-order-system/CMakeLists.txt;79;add_test;C:/Users/andre/source/repos/solid-ecommerce-order-system/CMakeLists.txt;0;")
+subdirs("_deps/googletest-build")
