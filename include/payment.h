72f56ac5efc54eaed00780f92f44b0faa3ab7661
Andrew <104359161+Andrew7441@users.noreply.github.com>
2026-01-14 17:01:15 +0200

    Add payment implementation

---
+++ include/payment.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+
+class Ipaymentprocessor { // Interface 
+public:
+	virtual std::string processPayment(double amount) = 0;
+	virtual ~Ipaymentprocessor() = default;
+
+};
+
+class PaymentProcessor : public Ipaymentprocessor {
+private:
+	static int nextId;
+
+public:	
+	virtual std::string processPayment(double amount);
+
+};
---
+++ src/payment_processor.cpp
@@ -0,0 +1,9 @@
+#include "payment.h"
+#include <string>
+
+int PaymentProcessor::nextId = 1;
+
+std::string PaymentProcessor::processPayment(double amount) {
+	return "$" + std::to_string(nextId++);
+}
+
